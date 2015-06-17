bf5b9124dd49fdf82e9aaa40702a65bc9b6cbdd6
carlosdiaz <alberto.carlos@gmail.com>
2015-06-17 11:45:48 -0500

    First app version

---
+++ app.py
@@ -0,0 +1,42 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, abort, make_response
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+sendPinResp = [{'resultCode':0, 'resultDesc':1}]
+
+@app.route('/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+
+@app.route('/api/hubmx/sendPIN', methods=['GET'])
+def send_pin_json():
+    if request.json:
+	return jsonify({sendPinREsp})
+    else: 
+	abort(404)
+	
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+if __name__ == '__main__':
+    app.run(debug=True)
