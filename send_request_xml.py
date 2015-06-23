8ee752b166e5c26c2c4903f834e05804bce9d452
carlosdiaz <alberto.carlos@gmail.com>
2015-06-23 13:33:14 -0500

    New request definition

--- send_request_xml.py
+++ send_request_xml.py
@@ -12,12 +12,24 @@ starttime = datetime.now()
 print "Start Time: " + str(starttime)
 
 
-request_xml =  """<?xml version="1.0" ?>
-<!DOCTYPE message SYSTEM "/home/jinny/bin/ems.dtd">
-<sendpin>
+request_xml = """<?xml version="1.0" encoding="ISO-8859-1"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
+xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns8156="http://tempuri.org"><SOAP-ENV:Header>
+<tns:RequestSOAPHeader xmlns:tns="http://www.huawei.com/schema/osg/common/v2_1"><tns:AppId>N</tns:AppId>
+<tns:TransId>2015062300100779801</tns:TransId><tns:OA></tns:OA><tns:FA></tns:FA></tns:RequestSOAPHeader>
+<wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+<wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
+<wsse:Username>XXX</wsse:Username>
+<wsse:Password Type="...#PasswordDigest">XXX</wsse:Password>
+<wsse:Nonce>XXX</wsse:Nonce><wsse:Created>2015-06-23T00:10:07Z</wsse:Created></wsse:UsernameToken></wsse:Security>
+</SOAP-ENV:Header>
+<SOAP-ENV:Body>
+<sendPIN xmlns="http://www.csapi.org/schema/parlayx/sendpin/v1_0/local">
 <msisdn>123456</msisdn>
-<productid>9876543</productid>
-</sendpin>"""
+<productID>123456</productID>
+</sendPIN>
+</SOAP-ENV:Body>
+</SOAP-ENV:Envelope>"""
 
 print "Request: "+request_xml
 
