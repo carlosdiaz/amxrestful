ea5f67a9ebdd710585e5acebd8ab9c304e2d93d5
carlosdiaz <alberto.carlos@gmail.com>
2015-06-27 20:37:46 -0500

    Request for send pin

--- send_request_xml.py
+++ send_request_xml.py
@@ -3,6 +3,8 @@ import httplib
 from datetime import datetime
 
 
+#Request for SendPin method
+
 hostname = "127.0.0.1"
 uri = "api/hubmx/sendPIN"
 port = 5000
@@ -12,9 +14,11 @@ starttime = datetime.now()
 print "Start Time: " + str(starttime)
 
 
-request_xml = """<?xml version="1.0" encoding="ISO-8859-1"?><SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
+request_xml = """<?xml version="1.0" encoding="ISO-8859-1"?>
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" 
 xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns8156="http://tempuri.org"><SOAP-ENV:Header>
+xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns8156="http://tempuri.org">
+<SOAP-ENV:Header>
 <tns:RequestSOAPHeader xmlns:tns="http://www.huawei.com/schema/osg/common/v2_1"><tns:AppId>N</tns:AppId>
 <tns:TransId>2015062300100779801</tns:TransId><tns:OA></tns:OA><tns:FA></tns:FA></tns:RequestSOAPHeader>
 <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
@@ -25,11 +29,13 @@ xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:ns8156="http://
 </SOAP-ENV:Header>
 <SOAP-ENV:Body>
 <sendPIN xmlns="http://www.csapi.org/schema/parlayx/sendpin/v1_0/local">
-<msisdn>123456</msisdn>
+<msisdn>12345678</msisdn>
 <productID>123456</productID>
 </sendPIN>
 </SOAP-ENV:Body>
 </SOAP-ENV:Envelope>"""
+
+
 
 print "Request: "+request_xml
 
